0b972ec3e316047b737bd7bf9121772f959686ed
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-10-26 16:33:51 +0100

    generate map

--- frontend/src/app/(stage)/map.tsx
+++ frontend/src/app/(stage)/map.tsx
@@ -1,5 +1,50 @@
+'use client';
+
+import { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 export default function Map(
-  { className }: { className?: string }
 ): JSX.Element {
-  return <div className={className} id="map-container">Map</div>;
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+
+  const INITIAL_CENTER: [number, number] = [0, 51.5];
+  const INITIAL_ZOOM = 9;
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN ?? '';
+
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current!,
+      center: INITIAL_CENTER,
+      zoom: INITIAL_ZOOM,
+    });
+
+    return () => mapRef.current?.remove();
+  });
+
+  const handleButtonClick = () => {
+    mapRef.current?.flyTo({
+      center: INITIAL_CENTER,
+      zoom: INITIAL_ZOOM
+    })
+  }
+
+  return (
+    <>
+      <button
+        onClick={handleButtonClick}
+        className="absolute top-2 left-2 z-10 bg-white p-2 rounded-md shadow-md"
+      >
+        Reset
+      </button>
+      <div
+        className="h-full bg-green-100"
+        id="map-container"
+        ref={mapContainerRef}
+      />
+    </>
+  );
 }
