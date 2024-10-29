eb5925d7b0315a407e5d5df7c2b6f325d737c1ae
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-10-29 12:48:58 +0000

    add code to simplify scanning of enums

---
+++ backend/db/enum.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"errors"
+	"fmt"
+)
+
+type EnumValue interface {
+	~string
+}
+
+type EnumMap[T EnumValue] map[string]T
+
+func ScanEnum[T EnumValue](dest *T, src any, mapping EnumMap[T]) error {
+	if src == nil {
+		return errors.New("null value")
+	}
+
+	switch v := src.(type) {
+	case string:
+		if val, ok := mapping[v]; ok {
+			*dest = val
+			return nil
+		}
+		return fmt.Errorf("unsupported value: %s", v)
+	case []byte:
+		return ScanEnum(dest, string(v), mapping)
+	default:
+		return fmt.Errorf("unsupported value: %v", src)
+	}
+}
