1a82231ce21671436fe87d5dc8217be8068e00ef
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-10-29 22:29:03 +0000

    add functions to get connection and pool

--- backend/db/db.go
+++ backend/db/db.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	"context"
+	"os"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type DBConn interface {
@@ -26,4 +28,24 @@ func (q *Queries) WithConn(conn DBConn) *Queries {
 
 func New(conn DBConn) *Queries {
 	return &Queries{conn: conn}
+}
+
+func GetPool() (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(
+		context.Background(), os.Getenv("DATABASE_URL"),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return pool, nil
+}
+
+func GetConn() (DBConn, error) {
+	conn, err := pgx.Connect(
+		context.Background(), os.Getenv("DATABASE_URL"),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
