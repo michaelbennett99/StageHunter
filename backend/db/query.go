2767e1df324bddee2c8428ea1f9bbf9609b0fdd5
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-10-29 22:29:21 +0000

    write test queries

--- backend/db/query.go
+++ backend/db/query.go
@@ -6,11 +6,12 @@ import (
 
 const getDailyStageQuery = `
 SELECT stage_id FROM racedata.daily
-WHERE daily_id = (SELECT MAX(daily_id) FROM racedata.daily);
+WHERE daily_id = (SELECT MAX(daily_id) FROM racedata.daily)
+LIMIT 1;
 `
 
-func (q *Queries) GetDailyStage(ctx context.Context) (StageID, error) {
-	var id StageID
+func (q *Queries) GetDailyStage(ctx context.Context) (int, error) {
+	var id int
 	row := q.conn.QueryRow(ctx, getDailyStageQuery)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
@@ -22,8 +23,8 @@ const getRandomStageQuery = `
 SELECT racedata.get_random_stage_id();
 `
 
-func (q *Queries) GetRandomStage(ctx context.Context) (StageID, error) {
-	var id StageID
+func (q *Queries) GetRandomStage(ctx context.Context) (int, error) {
+	var id int
 	row := q.conn.QueryRow(ctx, getRandomStageQuery)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
@@ -41,12 +42,13 @@ SELECT
 	stage_end,
 	stage_length
 FROM racedata.races_stages
-WHERE stage_id = $1;
+WHERE stage_id = $1
+LIMIT 1;
 `
 
 // Get descriptive information about a stage
 func (q *Queries) GetStageInfo(
-	ctx context.Context, stageID StageID,
+	ctx context.Context, stageID int,
 ) (StageInfo, error) {
 	var info StageInfo
 	row := q.conn.QueryRow(ctx, getStageInfoQuery, stageID)
@@ -62,4 +64,91 @@ func (q *Queries) GetStageInfo(
 		return StageInfo{}, err
 	}
 	return info, nil
+}
+
+const getTrackQuery = `
+SELECT ST_AsGeoJSON(the_geom, 6)
+FROM racedata.stages s
+JOIN geog.tracks t ON s.gpx_id = t.track_id
+WHERE s.stage_id = $1
+LIMIT 1;
+`
+
+// Get the stage track as a GeoJSON object
+func (q *Queries) GetTrack(
+	ctx context.Context, stageID int,
+) (string, error) {
+	var track string
+	row := q.conn.QueryRow(ctx, getTrackQuery, stageID)
+	if err := row.Scan(&track); err != nil {
+		return "", err
+	}
+	return track, nil
+}
+
+const getElevationProfileQuery = `
+SELECT distance, ele
+FROM racedata.stages_elevation
+WHERE stage_id = $1
+ORDER BY distance;
+`
+
+func (q *Queries) GetElevationProfile(
+	ctx context.Context, stageID int,
+) ([]ElevationPoint, error) {
+	rows, err := q.conn.Query(
+		ctx, getElevationProfileQuery, stageID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	points := []ElevationPoint{}
+	for rows.Next() {
+		var point ElevationPoint
+		if err := rows.Scan(
+			&point.Distance, &point.Elevation,
+		); err != nil {
+			return nil, err
+		}
+		points = append(points, point)
+	}
+	return points, nil
+}
+
+const getResultsQuery = `
+SELECT * FROM racedata.get_results($1, $2);
+`
+
+type ResultsQueryParams struct {
+	StageID int
+	TopN    int
+}
+
+func (q *Queries) GetResults(
+	ctx context.Context, params ResultsQueryParams,
+) ([]Result, error) {
+	rows, err := q.conn.Query(ctx, getResultsQuery, params.StageID, params.TopN)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	results := []Result{}
+	for rows.Next() {
+		var result Result
+		if err := rows.Scan(
+			&result.Rank,
+			&result.Name,
+			&result.Team,
+			&result.Time,
+			&result.Points,
+			&result.Classification,
+		); err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+	return results, nil
 }
