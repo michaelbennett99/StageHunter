26ecd8c71b6379f9a32ba899fcc4213ca5e40a3e
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-11-03 23:28:39 +0000

    vacuum db after initialising

--- db/initdb.d/14_initdb-stagehunter.sh
+++ db/initdb.d/14_initdb-stagehunter.sh
@@ -6,6 +6,9 @@ export dump_file=/docker-entrypoint-dumps/dump.sql
 if [ -f "$dump_file" ]; then
     echo "Restoring from $dump_file and ignoring all errors"
     psql -f "$dump_file"
+
+    # Vacuum the database cluster to analyze and reclaim disk space
+    vacuumdb -a -z
 else
     echo "No dump file found"
     exit 1
