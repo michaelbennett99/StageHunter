eb267cc49c787a5e6ce73dc14abb8299d2a806a0
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-11-06 16:11:58 +0000

    switch to 200m gradient resolution

--- frontend/src/app/stage/stage.tsx
+++ frontend/src/app/stage/stage.tsx
@@ -33,7 +33,7 @@ export default async function Stage({
 }): Promise<JSX.Element> {
   const [track, gradientData] = await Promise.all([
     getTrack(stageId),
-    getGradientData(stageId, 100)
+    getGradientData(stageId, 200)
   ]);
   return (
     <div className="min-h-screen flex flex-col">
