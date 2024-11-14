3a87e3e88a929e0124314772b2ba750d4a7b61ea
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-11-14 11:25:41 +0000

    add backbone of code to verify answers

--- frontend/src/components/input/input.tsx
+++ frontend/src/components/input/input.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { useState, ChangeEventHandler } from 'react';
-import { FaCheck } from 'react-icons/fa';
+import { FaCheck, FaTimes, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import { numToRank } from '@/utils/utils';
 import Autocomplete from '@/components/autocomplete';
+import { useDecrement } from './hooks';
 
 export interface Options {
   grand_tours: string[];
@@ -13,23 +14,72 @@ export interface Options {
 }
 
 export default function Input(
-  { options }: { options: Options }
+  { stageId, options }: { stageId: string | number; options: Options }
 ): JSX.Element {
+  const infoValidationURL = `/api/stage/info/verify/${stageId}`;
+  const resultsValidationURL = `/api/stage/results/verify/${stageId}`;
+
   // Get all the entries in the data object and list them
   // If the entry is an array, it's a group of entries, so we group them
   // in an InputBoxGroup
   const list_elements = [
-    <InputBox name="Grand Tour" options={options.grand_tours} />,
-    <InputBox name="Year" />,
-    <InputBox name="Number" />,
-    <InputBox name="Start" />,
-    <InputBox name="End" />,
-    <InputBoxGroup name="Stage Results" nBoxes={3} options={options.riders} />,
-    <InputBoxGroup name="GC Results" nBoxes={3} options={options.riders} />,
-    <InputBoxGroup name="Points Results" nBoxes={3} options={options.riders} />,
-    <InputBoxGroup name="KOM Results" nBoxes={3} options={options.riders} />,
-    <InputBoxGroup name="Youth Results" nBoxes={3} options={options.riders} />,
-    <InputBoxGroup name="Teams Results" nBoxes={3} options={options.teams} />,
+    <InputBox
+      name="Grand Tour"
+      validationURL={`${infoValidationURL}?f=grand_tour`}
+      options={options.grand_tours}
+    />,
+    <InputBox
+      name="Year"
+      validationURL={`${infoValidationURL}?f=year`}
+    />,
+    <InputBox
+      name="Number"
+      validationURL={`${resultsValidationURL}?f=stage_no`}
+    />,
+    <InputBox
+      name="Start"
+      validationURL={`${infoValidationURL}?f=stage_start`}
+    />,
+    <InputBox
+      name="End"
+      validationURL={`${infoValidationURL}?f=stage_end`}
+    />,
+    <InputBoxGroup
+      name="Stage Results"
+      nBoxes={3}
+      validationURL={`${resultsValidationURL}?c=stage`}
+      options={options.riders}
+    />,
+    <InputBoxGroup
+      name="GC Results"
+      nBoxes={3}
+      validationURL={`${resultsValidationURL}?c=general`}
+      options={options.riders}
+    />,
+    <InputBoxGroup
+      name="Points Results"
+      nBoxes={3}
+      validationURL={`${resultsValidationURL}?c=points`}
+      options={options.riders}
+    />,
+    <InputBoxGroup
+      name="KOM Results"
+      nBoxes={3}
+      validationURL={`${resultsValidationURL}?c=mountains`}
+      options={options.riders}
+    />,
+    <InputBoxGroup
+      name="Youth Results"
+      nBoxes={3}
+      validationURL={`${resultsValidationURL}?c=youth`}
+      options={options.riders}
+    />,
+    <InputBoxGroup
+      name="Teams Results"
+      nBoxes={3}
+      validationURL={`${resultsValidationURL}?c=teams`}
+      options={options.teams}
+    />,
   ].map((element) => (
     <li key={element.props.name}>{element}</li>
   ));
@@ -67,9 +117,10 @@ function BoxList(
 }
 
 function InputBoxGroup(
-  { name, nBoxes, options }: {
+  { name, nBoxes, validationURL, options }: {
     name: string;
     nBoxes: number;
+    validationURL: string;
     options?: string[];
   }
 ): JSX.Element {
@@ -85,6 +136,7 @@ function InputBoxGroup(
           >
             <InputBox
               name={numToRank(i + 1) ?? ''}
+              validationURL={`${validationURL}&r=${i+1}`}
               options={options}
             />
           </li>
@@ -95,50 +147,92 @@ function InputBoxGroup(
 }
 
 function InputBox(
-  { name, options, inputType }: {
+  { name, validationURL, options, inputType }: {
     name: string;
+    validationURL: string;
     options?: string[];
     inputType?: 'text' | 'number';
   }
 ): JSX.Element {
   const [val, setVal] = useState('');
+  const [isCorrect, setIsCorrect] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [tries, decrementTries] = useDecrement(1);
+
+  const handleSubmit = async () => {
+    if (isCorrect || tries <= 0 || val === '') return;
+
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${validationURL}&v=${val}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data: boolean = await response.json();
+      setIsCorrect(data);
+      decrementTries();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <div className="flex items-center w-full h-full gap-1">
       <div className="flex-grow text-black text-sm">{name}</div>
       <TextInput
-        name={name}
         value={val}
+        tries={tries}
+        isLoading={isLoading}
+        isCorrect={isCorrect}
         onChange={(e) => setVal(e.target.value)}
+        onSubmit={handleSubmit}
         options={options}
-        inputType={inputType}
       />
     </div>
   );
 }
 
+function ButtonText({ tries, isCorrect, isLoading }: {
+  tries: number;
+  isCorrect: boolean;
+  isLoading: boolean;
+}): JSX.Element {
+  if (isLoading) return <FaSpinner className="h-full w-full animate-spin" />;
+  if (tries <= 0 && !isCorrect) return <FaTimes className="h-full w-full" />;
+  if (isCorrect) return <FaCheck className="h-full w-full" />;
+  return <FaPlus className="h-full w-full" />;
+}
+
 // A text input box that checks if the input is correct and disables
 // itself if it is
-function TextInput( {
-  name,
+function TextInput({
   value,
+  tries,
+  isLoading,
+  isCorrect,
   onChange,
+  onSubmit,
   options,
-  inputType = 'text',
   className,
 }: {
-  name: string;
   value: string;
+  tries: number;
+  isLoading: boolean;
+  isCorrect: boolean;
   onChange: ChangeEventHandler<HTMLInputElement>;
+  onSubmit: () => Promise<void>;
   options?: string[];
-  inputType?: 'text' | 'number';
   className?: string;
 }): JSX.Element {
-  const listId = `options-${name}`;
   return (
     <form
       className="flex items-center h-full gap-1"
-      onSubmit={(e) => e.preventDefault()}
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit();
+      }}
     >
       {options ? (
         <Autocomplete
@@ -153,20 +247,19 @@ function TextInput( {
             border-gray-300 text-black rounded-md
             ${className}`
           }
-          type={inputType}
+          type="text"
           value={value}
           onChange={onChange}
-          list={listId}
         />
       )}
       <button
-        type="button"
+        type="submit"
         className="
           bg-blue-500 text-white p-1 rounded-md h-full w-full
           hover:bg-blue-700
         "
       >
-        <FaCheck className="h-full w-full" />
+        <ButtonText tries={tries} isCorrect={isCorrect} isLoading={isLoading} />
       </button>
     </form>
   );
