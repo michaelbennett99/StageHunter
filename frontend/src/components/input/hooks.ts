224587b2e8ffcc9da77e44bace1812d7170241d0
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-11-14 16:34:08 +0000

    Add a useBomb hook for single time boolean switch

--- frontend/src/components/input/hooks.ts
+++ frontend/src/components/input/hooks.ts
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 
+/**
+ * A hook that returns a number and a function to increment it.
+ * @param start the starting number
+ * @param step the amount to increment by
+ * @returns a number and a function to increment it
+ */
 export function useIncrement(
   start: number = 0, step: number = 1
 ): [number, () => void] {
@@ -11,4 +17,14 @@ export function useDecrement(
   start: number = 0, step: number = 1
 ): [number, () => void] {
   return useIncrement(start, -step);
+}
+
+/**
+ * A hook that returns a boolean and a function to set it to be true. Once
+ * the boolean bomb is set to true, it can never be unset.
+ * @returns a boolean and a function to set it to true
+ */
+export function useBomb(): [boolean, () => void] {
+  const [value, setValue] = useState(false);
+  return [value, () => setValue(true)];
 }
