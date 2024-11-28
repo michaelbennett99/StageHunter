3749f60e41524ea69195deb30303b52f0983379e
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-11-28 13:50:15 +0000

    restart and then run nginx in foreground

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -6,3 +6,9 @@ nginx
 # Run certbot first
 certbot --nginx -n --agree-tos -m admin@stagehunter.cc \
     -d stagehunter.cc -d www.stagehunter.cc
+
+# Stop nginx
+nginx -s stop
+
+# Then start nginx in foreground
+nginx -g 'daemon off;'
