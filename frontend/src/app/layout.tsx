51c06505e787a79be79241a9a19da23ad1ca7894
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-17 14:53:37 +0000

    use h-dvh rather than h-screen

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col h-screen overflow-hidden antialiased">
+      <body className="flex flex-col h-dvh overflow-hidden antialiased">
         <Header />
         {children}
       </body>
