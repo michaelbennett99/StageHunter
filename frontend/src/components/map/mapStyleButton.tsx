bfc39ed8d8e2c97e65c5f23959b26216c3558934
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-19 15:26:44 +0000

    try style button using Select

--- frontend/src/components/map/mapStyleButton.tsx
+++ frontend/src/components/map/mapStyleButton.tsx
@@ -2,18 +2,13 @@ import { useState, useEffect } from 'react';
 import { LuChevronDown, LuCheck } from 'react-icons/lu';
 
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList
-} from '@/components/ui/command';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger
-} from '@/components/ui/popover';
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectItem
+} from '@/components/ui/select';
 
 import {
   mapboxStyleMap,
@@ -32,13 +27,13 @@ export default function MapStyleButton(
 ): JSX.Element {
   const { mapRef, defaultStyle, ...buttonProps } = props;
 
-  const [open, setOpen] = useState(false);
-  const [selectedStyle, setSelectedStyle] = useState<MapboxStyleId>(defaultStyle);
+  const [selectedStyle, setSelectedStyle] = useState<MapboxStyleId>(
+    defaultStyle
+  );
   const fullMapboxStyle = mapboxStyleMap[selectedStyle];
 
   const handleSelectStyle = (styleId: MapboxStyleId) => {
     setSelectedStyle(styleId);
-    setOpen(false);
   };
 
   // Set map style when selected style changes
@@ -48,40 +43,27 @@ export default function MapStyleButton(
 
   return (
     <MapButton {...buttonProps} asChild>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <button
-            role="combobox"
-            aria-expanded={open}
-            className="justify-between flex items-center gap-1 p-2 rounded-md shadow-md group bg-background text-sm"
+      <Select defaultValue={selectedStyle} onValueChange={handleSelectStyle}>
+        <SelectTrigger className="p-2 rounded-md shadow-md group bg-background text-sm">
+          <SelectValue
+            className="justify-between flex items-center gap-1"
           >
             <span>{fullMapboxStyle.name}</span>
-            <LuChevronDown />
-          </button>
-        </PopoverTrigger>
-        <PopoverContent className="p-0 w-52">
-          <Command>
-            <CommandInput placeholder="Search style..." />
-            <CommandList>
-              <CommandEmpty>No style found</CommandEmpty>
-              <CommandGroup>
-                {mapboxStyles.map((style) => (
-                  <CommandItem
-                    key={style.id}
-                    value={style.id}
-                    onSelect={() => {
-                      handleSelectStyle(style.id);
-                    }}
-                  >
-                    <span>{style.name}</span>
-                    {selectedStyle === style.id && <LuCheck />}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent className="p-0 w-52">
+          <SelectGroup>
+            {mapboxStyles.map((style) => (
+              <SelectItem
+                key={style.id}
+                value={style.id}
+              >
+                <span>{style.name}</span>
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </MapButton>
   )
 }
