ca572413575ade9035d156308c6c853049898d5a
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-22 19:48:55 +0000

    edit select component

--- frontend/src/components/map/mapStyleButton.tsx
+++ frontend/src/components/map/mapStyleButton.tsx
@@ -1,7 +1,14 @@
 import { Dispatch, SetStateAction } from 'react';
 import { LuLayers } from 'react-icons/lu';
 
-import * as Select from '@radix-ui/react-select'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
 
 import {
   mapboxStyles,
@@ -26,33 +33,27 @@ export default function MapStyleButton(
   };
 
   return (
-    <Select.Root defaultValue={selectedStyle} onValueChange={handleSelectStyle}>
-      <Select.Trigger
+    <Select defaultValue={selectedStyle} onValueChange={handleSelectStyle}>
+      <SelectTrigger
         className="p-2 rounded-md shadow-md group bg-background text-sm"
         asChild
       >
         <MapButton>
           <LuLayers className="w-4 h-4" />
         </MapButton>
-      </Select.Trigger>
-      <Select.Content
-        className={cn(
-          "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
-          "p-1 w-52"
-        )}
-      >
-        <Select.Group>
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
           {mapboxStyles.map((style) => (
-            <Select.Item
+            <SelectItem
               key={style.id}
               value={style.id}
-              className="p-2 rounded-md shadow-md group bg-background text-sm hover:bg-accent hover:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
             >
               <span>{style.name}</span>
-            </Select.Item>
+            </SelectItem>
           ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 }
