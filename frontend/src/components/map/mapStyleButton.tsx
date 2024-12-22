fce8f37ef5cbe68cba2422f833a21a92b406e56f
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-22 19:44:56 +0000

    extra styling

--- frontend/src/components/map/mapStyleButton.tsx
+++ frontend/src/components/map/mapStyleButton.tsx
@@ -8,6 +8,7 @@ import {
   type MapboxStyleId
 } from '@/interfaces/mapboxStyles';
 import MapButton, { MapButtonProps } from './mapButton';
+import { cn } from '@/lib/utils';
 
 export type MapStyleButtonProps = MapButtonProps & {
   mapRef: React.RefObject<mapboxgl.Map>,
@@ -34,13 +35,18 @@ export default function MapStyleButton(
           <LuLayers className="w-4 h-4" />
         </MapButton>
       </Select.Trigger>
-      <Select.Content className="p-0 w-52 bg-background">
+      <Select.Content
+        className={cn(
+          "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
+          "p-1 w-52"
+        )}
+      >
         <Select.Group>
           {mapboxStyles.map((style) => (
             <Select.Item
               key={style.id}
               value={style.id}
-              className="p-2 rounded-md shadow-md group bg-background text-sm"
+              className="p-2 rounded-md shadow-md group bg-background text-sm hover:bg-accent hover:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
             >
               <span>{style.name}</span>
             </Select.Item>
