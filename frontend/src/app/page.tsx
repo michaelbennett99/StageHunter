12ec7bf50ae1e49d0b8011f7f92005f6b2b58608
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-23 12:10:24 +0000

    style

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,10 +7,19 @@ export const revalidate = 3600; // 1 hour
 export default async function Page(): Promise<JSX.Element> {
   const dailyStage = await serverApiClient.getDailyStage();
 
+  const dateString = dailyStage.date.toLocaleDateString(
+    undefined, {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    }
+  );
+
   return (
     <main className="flex flex-col flex-1 min-h-0">
       <Stage stageId={dailyStage.stage_id} />
-      <HeaderInfoSetter headerInfo={dailyStage.date.toLocaleDateString()} />
+      <HeaderInfoSetter headerInfo={dateString} />
     </main>
   );
 }
